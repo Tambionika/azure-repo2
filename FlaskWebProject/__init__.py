64f2d08b32e6692f210739bab2974233d810345a
Tambionika <153222830+Tambionika@users.noreply.github.com>
2025-05-09 19:29:31 -0300

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,32 +1,12 @@
 """
 The flask application package.
 """
+import logging
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
-from FlaskWebProject import app
-
-from datetime import datetime
-from flask import render_template, flash, redirect, request, session, url_for
-from werkzeug.urls import url_parse
-from config import Config
-from FlaskWebProject import app, db
-from FlaskWebProject.forms import LoginForm, PostForm
-from flask_login import current_user, login_user, logout_user, login_required
-from FlaskWebProject.models import User, Post
-import msal
-import uuid
-import logging
-from opencensus.ext.azure.log_exporter import AzureLogHandler
-
-# Configure logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-# Add Azure Log Handler
-logger.addHandler(AzureLogHandler(connection_string='InstrumentationKey=b1b86dcd-6a39-4420-86e6-3b6ca6acb0fe'))
 
 app = Flask(__name__)
 app.config.from_object(Config)
