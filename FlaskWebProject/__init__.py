68a8a238275bb7f4466ccdb0247647720d14e539
Tambionika <153222830+Tambionika@users.noreply.github.com>
2025-05-09 18:59:01 -0300

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -2,8 +2,6 @@
 The flask application package.
 """
 import logging
-from logging.handlers import RotatingFileHandler
-import os
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -12,22 +10,10 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
+# TODO: Add any logging levels and handlers with app.logger
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
-
-# Configure logging
-if not app.debug:
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/udacitycms.log', maxBytes=10240, backupCount=10)
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-
-    app.logger.setLevel(logging.INFO)
-    app.logger.info('UdacityCMS startup')
 
 import FlaskWebProject.views
